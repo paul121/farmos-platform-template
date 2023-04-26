1e863b14ffa097a25061f7349ddd15b510527c16
Paul Weidner <paul.weidner@gmail.com>
2023-04-25 22:37:14 -0700

    Clean settings.php files

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -4,7 +4,7 @@
 
 /**
  * @file
- * Platform.sh example settings.php file for Drupal 8.
+ * Platform.sh settings.php file for Drupal 9+.
  */
 
 // Default Drupal settings.
@@ -31,10 +31,8 @@ $settings['file_scan_ignore_directories'] = [
 // $settings['hash_salt'] = 'change_me';
 
 // Set up a config sync directory.
-//
 // This is defined inside the read-only "config" directory, deployed via Git.
 $settings['config_sync_directory'] = '../config/sync';
-
 
 // Automatic Platform.sh settings.
 if (file_exists($app_root . '/' . $site_path . '/settings.platformsh.php')) {
@@ -46,20 +44,7 @@ if (file_exists(__DIR__ . '/settings.ddev.php') && getenv('IS_DDEV_PROJECT') == 
   include __DIR__ . '/settings.ddev.php';
 }
 
-/**
- * Load local development override configuration, if available.
- *
- * Create a settings.local.php file to override variables on secondary (staging,
- * development, etc.) installations of this site.
- *
- * Typical uses of settings.local.php include:
- * - Disabling caching.
- * - Disabling JavaScript/CSS compression.
- * - Rerouting outgoing emails.
- *
- * Keep this code block at the end of this file to take full effect.
- */
-
+// Local development.
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
